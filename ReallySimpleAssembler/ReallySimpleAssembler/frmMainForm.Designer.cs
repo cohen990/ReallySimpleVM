30d6dde2f4e1929026c338ab3a5809d8896d43dc
Dan Cohen <dan.cohen@euromoneyplc.com>
2015-09-05 16:39:26 +0100

    Add gui and assembler
    Some samples are coming along for the ride

---
+++ ReallySimpleAssembler/ReallySimpleAssembler/frmMainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ReallySimpleAssembler
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtSourceFileName = new System.Windows.Forms.TextBox();
+            this.txtOutputFileName = new System.Windows.Forms.TextBox();
+            this.txtOrigin = new System.Windows.Forms.TextBox();
+            this.btnAssemble = new System.Windows.Forms.Button();
+            this.btnOutputBrowse = new System.Windows.Forms.Button();
+            this.btnSourceBrowse = new System.Windows.Forms.Button();
+            this.fdDestinationFile = new System.Windows.Forms.OpenFileDialog();
+            this.fdSourceFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(16, 32);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(63, 13);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Source File:";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(20, 52);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(61, 13);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "Output File:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(44, 77);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(37, 13);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Origin:";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(77, 77);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(13, 13);
+            this.Label4.TabIndex = 3;
+            this.Label4.Text = "$";
+            // 
+            // txtSourceFileName
+            // 
+            this.txtSourceFileName.Location = new System.Drawing.Point(87, 20);
+            this.txtSourceFileName.Name = "txtSourceFileName";
+            this.txtSourceFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtSourceFileName.TabIndex = 4;
+            // 
+            // txtOutputFileName
+            // 
+            this.txtOutputFileName.Location = new System.Drawing.Point(87, 49);
+            this.txtOutputFileName.Name = "txtOutputFileName";
+            this.txtOutputFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtOutputFileName.TabIndex = 5;
+            // 
+            // txtOrigin
+            // 
+            this.txtOrigin.Location = new System.Drawing.Point(87, 75);
+            this.txtOrigin.Name = "txtOrigin";
+            this.txtOrigin.Size = new System.Drawing.Size(100, 20);
+            this.txtOrigin.TabIndex = 6;
+            // 
+            // btnAssemble
+            // 
+            this.btnAssemble.Location = new System.Drawing.Point(97, 138);
+            this.btnAssemble.Name = "btnAssemble";
+            this.btnAssemble.Size = new System.Drawing.Size(75, 23);
+            this.btnAssemble.TabIndex = 7;
+            this.btnAssemble.Text = "Assemble!";
+            this.btnAssemble.UseVisualStyleBackColor = true;
+            this.btnAssemble.Click += new System.EventHandler(this.btnAssemble_Click);
+            // 
+            // btnOutputBrowse
+            // 
+            this.btnOutputBrowse.Location = new System.Drawing.Point(192, 46);
+            this.btnOutputBrowse.Name = "btnOutputBrowse";
+            this.btnOutputBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnOutputBrowse.TabIndex = 8;
+            this.btnOutputBrowse.Text = "Browse...";
+            this.btnOutputBrowse.UseVisualStyleBackColor = true;
+            this.btnOutputBrowse.Click += new System.EventHandler(this.btnOutputBrowse_Click);
+            // 
+            // btnSourceBrowse
+            // 
+            this.btnSourceBrowse.Location = new System.Drawing.Point(193, 17);
+            this.btnSourceBrowse.Name = "btnSourceBrowse";
+            this.btnSourceBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnSourceBrowse.TabIndex = 9;
+            this.btnSourceBrowse.Text = "Browse...";
+            this.btnSourceBrowse.UseVisualStyleBackColor = true;
+            this.btnSourceBrowse.Click += new System.EventHandler(this.btnSourceBrowse_Click);
+            // 
+            // fdDestinationFile
+            // 
+            this.fdDestinationFile.DefaultExt = "B32";
+            this.fdDestinationFile.Filter = "B32 Files|*.B32";
+            // 
+            // fdSourceFile
+            // 
+            this.fdSourceFile.DefaultExt = "asm";
+            this.fdSourceFile.Filter = "B32 Assembly Files|*.asm";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 176);
+            this.Controls.Add(this.btnSourceBrowse);
+            this.Controls.Add(this.btnOutputBrowse);
+            this.Controls.Add(this.btnAssemble);
+            this.Controls.Add(this.txtOrigin);
+            this.Controls.Add(this.txtOutputFileName);
+            this.Controls.Add(this.txtSourceFileName);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Name = "frmMainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.TextBox txtSourceFileName;
+        private System.Windows.Forms.TextBox txtOutputFileName;
+        private System.Windows.Forms.TextBox txtOrigin;
+        private System.Windows.Forms.Button btnAssemble;
+        private System.Windows.Forms.Button btnOutputBrowse;
+        private System.Windows.Forms.Button btnSourceBrowse;
+        private System.Windows.Forms.OpenFileDialog fdDestinationFile;
+        private System.Windows.Forms.OpenFileDialog fdSourceFile;
+    }
+}
+
---
+++ ReallySimpleAssembler/ReallySimpleAssembler/frmMainForm.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ReallySimpleAssembler
+{
+    public partial class frmMainForm : Form
+    {
+        private string SourceProgram;
+        private Hashtable LabelTable;
+        private int CurrentNdx;
+        private ushort AsLength;
+        private bool IsEnd;
+        private ushort ExecutionAddress;
+
+        private enum Registers
+        {
+            Unknown = 0,
+            A = 4,
+            B = 2,
+            D = 1,
+            X = 16,
+            Y = 8
+        }
+
+        public frmMainForm()
+        {
+            InitializeComponent();
+
+            LabelTable = new Hashtable(50);
+            CurrentNdx = 0;
+            AsLength = 0;
+            ExecutionAddress = 0;
+            IsEnd = false;
+            SourceProgram = "";
+            txtOrigin.Text = "1000";
+        }
+
+        private void btnSourceBrowse_Click(object sender, EventArgs e)
+        {
+            fdSourceFile.ShowDialog();
+            txtSourceFileName.Text = fdSourceFile.FileName;
+        }
+
+        private void btnOutputBrowse_Click(object sender, EventArgs e)
+        {
+            fdDestinationFile.ShowDialog();
+            txtOutputFileName.Text = fdDestinationFile.FileName;
+        }
+
+        private void btnAssemble_Click(object sender, EventArgs e)
+        {
+            AsLength = Convert.ToUInt16(txtOrigin.Text, 16);
+            BinaryWriter output;
+            TextReader input;
+            FileStream fs = new FileStream(txtOutputFileName.Text, FileMode.Create);
+            output = new BinaryWriter(fs);
+            input = File.OpenText(txtSourceFileName.Text);
+            SourceProgram = input.ReadToEnd();
+            input.Close();
+            output.Write('B');
+            output.Write('3');
+            output.Write('2');
+            output.Write(Convert.ToUInt16(txtOrigin.Text, 16));
+            output.Write((ushort)0);
+            Parse(output);
+            output.Seek(5, SeekOrigin.Begin);
+            output.Write(ExecutionAddress);
+            output.Close();
+            fs.Close();
+            MessageBox.Show("Done!");
+        }
+
+        private void Parse(BinaryWriter OutputFile)
+        {
+            CurrentNdx = 0;
+            while (IsEnd == false)
+                LabelScan(OutputFile, true);
+            IsEnd = false;
+            CurrentNdx = 0;
+            AsLength = Convert.ToUInt16(txtOrigin.Text, 16);
+            while (IsEnd == false)
+                LabelScan(OutputFile, false);
+        }
+
+        private void LabelScan(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            if (char.IsLetter(SourceProgram[CurrentNdx]))
+            {
+                // Must be a label
+                if (IsLabelScan) LabelTable.Add(GetLabelName(), AsLength);
+                while (SourceProgram[CurrentNdx] != '\n')
+                    CurrentNdx++;
+                CurrentNdx++;
+                return;
+            }
+            EatWhiteSpaces();
+            ReadMneumonic(OutputFile, IsLabelScan);
+        }
+
+        // Added myself - be ready to delete if it's wrong.
+        private void EatWhiteSpaces()
+        {
+            while(char.IsWhiteSpace(SourceProgram[CurrentNdx]))
+            {
+                CurrentNdx++;
+            }
+        }
+
+        // Added myself - be ready to delete if it's wrong.
+        private string GetLabelName()
+        {
+            string lblname = "";
+            while (char.IsLetterOrDigit(SourceProgram[CurrentNdx]))
+            {
+                if (SourceProgram[CurrentNdx] == ':')
+                {
+                    CurrentNdx++;
+                    break;
+                }
+                lblname = lblname + SourceProgram[CurrentNdx];
+                CurrentNdx++;
+            }
+            return lblname.ToUpper();
+        }
+
+        private void ReadMneumonic(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            string Mneumonic = "";
+            while (!(char.IsWhiteSpace(SourceProgram[CurrentNdx])))
+            {
+                Mneumonic = Mneumonic + SourceProgram[CurrentNdx];
+                CurrentNdx++;
+            }
+            if (Mneumonic.ToUpper() == "LDX")
+                InterpretLDX(OutputFile, IsLabelScan);
+            if (Mneumonic.ToUpper() == "LDA")
+                InterpretLDA(OutputFile, IsLabelScan);
+            if (Mneumonic.ToUpper() == "STA")
+                InterpretSTA(OutputFile, IsLabelScan);
+            if (Mneumonic.ToUpper() == "END")
+            {
+                IsEnd = true;
+                DoEnd(OutputFile, IsLabelScan);
+                EatWhiteSpaces();
+                ExecutionAddress = (ushort)LabelTable[(GetLabelName())];
+                return;
+            }
+            while (SourceProgram[CurrentNdx] != '\n')
+            {
+                CurrentNdx++;
+            }
+            CurrentNdx++;
+        }
+
+        private void InterpretLDA(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            EatWhiteSpaces();
+            if (SourceProgram[CurrentNdx] == '#')
+            {
+                CurrentNdx++;
+                byte val = ReadByteValue();
+                AsLength += 2;
+                if (!IsLabelScan)
+                {
+                    OutputFile.Write((byte)0x01);
+                    OutputFile.Write(val);
+                }
+            }
+        }
+
+
+        private void InterpretLDX(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            EatWhiteSpaces();
+            if (SourceProgram[CurrentNdx] == '#')
+            {
+                CurrentNdx++;
+                ushort val = ReadWordValue();
+                AsLength += 3;
+                if (!IsLabelScan)
+                {
+                    OutputFile.Write((byte)0x02);
+                    OutputFile.Write(val);
+                }
+            }
+        }
+
+        private void InterpretSTA(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            EatWhiteSpaces();
+            if(SourceProgram[CurrentNdx] == ',')
+            {
+                Registers r;
+                byte opcode = 0x00;
+
+                CurrentNdx++;
+                EatWhiteSpaces();
+
+                r = ReadRegister();
+
+                switch (r)
+                {
+                    case Registers.X:
+                        opcode = 0x03;
+                        break;
+                }
+
+                AsLength += 1;
+
+                if (!IsLabelScan)
+                {
+                    OutputFile.Write(opcode);
+                }
+            }
+        }
+
+        private void DoEnd(BinaryWriter OutputFile, bool IsLabelScan)
+        {
+            AsLength++;
+            if (!IsLabelScan)
+            {
+                OutputFile.Write((byte)0x04);
+            }
+        }
+
+        private Registers ReadRegister()
+        {
+            Registers r = Registers.Unknown;
+
+            // How it should be done
+            //Enum.TryParse(SourceProgram[CurrentNdx].ToString(), out r);
+
+            // How it is being done
+            if ((SourceProgram[CurrentNdx] == 'X') ||
+            (SourceProgram[CurrentNdx] == 'x'))
+                r = Registers.X;
+            if ((SourceProgram[CurrentNdx] == 'Y') ||
+            (SourceProgram[CurrentNdx] == 'y'))
+                r = Registers.Y;
+            if ((SourceProgram[CurrentNdx] == 'D') ||
+            (SourceProgram[CurrentNdx] == 'd'))
+                r = Registers.D;
+            if ((SourceProgram[CurrentNdx] == 'A') ||
+            (SourceProgram[CurrentNdx] == 'a'))
+                r = Registers.A;
+            if ((SourceProgram[CurrentNdx] == 'B') ||
+            (SourceProgram[CurrentNdx] == 'b'))
+                r = Registers.B;
+
+            CurrentNdx++;
+            return r;
+        }
+
+        private ushort ReadWordValue()
+        {
+            ushort val = 0;
+            bool isHex = false;
+            string sval = "";
+
+            if (SourceProgram[CurrentNdx] == '$')
+            {
+                CurrentNdx++;
+                isHex = true;
+            }
+
+            while (char.IsLetterOrDigit(SourceProgram[CurrentNdx]))
+            {
+                sval = sval + SourceProgram[CurrentNdx];
+                CurrentNdx++;
+            }
+
+            if (isHex)
+            {
+                val = Convert.ToUInt16(sval, 16);
+            }
+            else
+            {
+                val = ushort.Parse(sval);
+            }
+
+            return val;
+        }
+
+        private byte ReadByteValue()
+        {
+            byte val = 0;
+            bool isHex = false;
+            string sval = "";
+
+            if (SourceProgram[CurrentNdx] == '$')
+            {
+                CurrentNdx++;
+                isHex = true;
+            }
+
+            while (char.IsLetterOrDigit(SourceProgram[CurrentNdx]))
+            {
+                sval = sval + SourceProgram[CurrentNdx];
+                CurrentNdx++;
+            }
+
+            if (isHex)
+            {
+                val = Convert.ToByte(sval, 16);
+            }
+            else
+            {
+                val = byte.Parse(sval);
+            }
+
+            return val;
+        }
+    }
+}
